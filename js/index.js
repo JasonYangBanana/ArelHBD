e71ab0e1935bd34cf629405eb33734a071f4d33c
JasonYang <jason19901207@gmail.com>
2018-01-08 18:55:11 +0800

    js重複宣告變數了

--- js/index.js
+++ js/index.js
@@ -7,10 +7,8 @@ submitButton.addEventListener('click', checkDate, false);
 function checkDate(){
 
     if (inputDate.value === "01/08"|| inputDate.value === "0108"|| inputDate.value === "01.08"){
-        var resultPic = document.getElementById('resultPic');
         resultPic.src = 'img/Arel.jpg';
     } else {
-        var resultPic = document.getElementById('resultPic');
         resultPic.src = 'img/MrChow.jpg';
     }
     return false;
